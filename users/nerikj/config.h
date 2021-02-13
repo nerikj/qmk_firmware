78938df8a03339664ec99291e184cb61f8a75ce7
Erik Nordlund <me@nilserikjohan.se>
2021-02-13 09:11:00 +0100

    Move home row aliases

--- users/nerikj/config.h
+++ users/nerikj/config.h
@@ -13,15 +13,3 @@
 
 // Recommended for heavy chording.
 #define QMK_KEYS_PER_SCAN 4
-
-
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
-#define HOME_X RALT_T(KC_X)
-#define HOME_J LSFT_T(KC_J)
-#define HOME_K LCTL_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN LGUI_T(KC_SCLN)
-#define HOME_DOT RALT_T(KC_DOT)
