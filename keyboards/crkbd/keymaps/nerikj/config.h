a61607921f0bc4aedef3be12368d24417925e60a
Erik Nordlund <me@nilserikjohan.se>
2021-12-27 20:42:02 +0100

    nerikj: Add layout for crkbd/r2g

---
+++ keyboards/crkbd/keymaps/nerikj/config.h
@@ -0,0 +1,42 @@
+// Copyright 2019 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#define XXX KC_NO
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+// #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+// try to prevent not waking up after sleep
+#define USB_SUSPEND_WAKEUP_DELAY 2000
+// #define NO_USB_STARTUP_CHECK // not tested yet
+
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+// #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+#define LAYOUT_miryoku(\
+       K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
+       K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
+       K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
+       N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39\
+)\
+LAYOUT_split_3x6_3(\
+XXX,   K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,   XXX,\
+XXX,   K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,   XXX,\
+XXX,   K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,   XXX,\
+                     K32,   K33,   K34,          K35,   K36,   K37\
+)
---
+++ keyboards/crkbd/keymaps/nerikj/rules.mk
@@ -0,0 +1,13 @@
+USER_NAME := manna-harbour_miryoku
+
+LTO_ENABLE = yes
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+# RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no
