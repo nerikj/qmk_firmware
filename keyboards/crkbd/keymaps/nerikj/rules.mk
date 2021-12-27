faa44fbeace15bad61c9b3a978e45519beb1ad5f
Erik Nordlund <me@nilserikjohan.se>
2021-12-27 20:42:02 +0100

    nerikj: Add layout for crkbd/r2g

---
+++ keyboards/crkbd/keymaps/nerikj/rules.mk
@@ -0,0 +1,12 @@
+USER_NAME := manna-harbour_miryoku
+
+LTO_ENABLE = yes
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
